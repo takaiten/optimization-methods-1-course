c069336d21f85e904cb8e993da3abd3e41bff212
takaiten <lopilopj@gmail.com>
2020-02-15 20:18:06 +0700

    Add minimum interval search method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
 
 EPS = 1e-07
+DELTA = EPS / 2
+
 SQRT_OF_5 = math.sqrt(5)
 # Golden ratio coefficients
 GR_COEFF_1 = (3 - SQRT_OF_5) / 2
@@ -15,7 +17,7 @@ def f(x):
 
 
 def dichotomy(a, b):
-    return (a + b - EPS / 2) / 2, (a + b + EPS / 2) / 2
+    return (a + b - DELTA) / 2, (a + b + DELTA) / 2
 
 
 def golden_ratio(a, b):
@@ -34,58 +36,89 @@ def fibonacci(a, b):
     return a + fibonacci_number(n) / fibonacci_number(n + 2) * (b - a), \
            a + fibonacci_number(n + 1) / fibonacci_number(n + 2) * (b - a)
 
-def write_to_file(string='', mode='w'):
+
+def write_to_file(string='', mode='w', path='output.txt'):
     try:
-        with open('input.txt', mode) as fout:
+        with open(path, mode) as fout:
             fout.write(string)
     except IOError:
         print('Error')
 
+
 def search_of_min(a, b, func):
-    
-    #write_to_file(str(a) + '\n', 'a')
-    #write_to_file(str(b) + '\n', 'a')
-    #write_to_file(str(b - a) + '\n', 'a')
-    
+    # write_to_file(str(a) + '\n', 'a')
+    # write_to_file(str(b) + '\n', 'a')
+    # write_to_file(str(b - a) + '\n', 'a')
+
     if (b - a) < EPS:
         return (a + b) / 2
 
     x1, x2 = func(a, b)
 
-    #write_to_file(str(x1) + '\n', 'a')
-    #write_to_file(str(x2) + '\n', 'a')
+    # write_to_file(str(x1) + '\n', 'a')
+    # write_to_file(str(x2) + '\n', 'a')
 
     f1 = f(x1)
     f2 = f(x2)
 
-    #write_to_file(str(f1) + '\n', 'a')
-    #write_to_file(str(f2) + '\n', 'a')
+    # write_to_file(str(f1) + '\n', 'a')
+    # write_to_file(str(f2) + '\n', 'a')
 
     if f1 == f2:
-        #write_to_file(str((x2-x1)/(b-a)) + '\n', 'a')
+        # write_to_file(str((x2-x1)/(b-a)) + '\n', 'a')
         return search_of_min(x1, x2, func)
     elif f1 < f2:
-        #write_to_file(str((x2-a)/(b-a)) + '\n', 'a')
+        # write_to_file(str((x2-a)/(b-a)) + '\n', 'a')
         return search_of_min(a, x2, func)
     else:
-        #write_to_file(str((b-x1)/(b-a)) + '\n', 'a')
+        # write_to_file(str((b-x1)/(b-a)) + '\n', 'a')
         return search_of_min(x1, b, func)
 
 
+def min_interval_search(x0):
+    x = [x0]
+    f0 = f(x[0])
+    h = DELTA
+
+    if f0 > f(x[0] + DELTA):
+        x.append(x0 + DELTA)
+        h *= 1
+    elif f0 > f(x[0] - DELTA):
+        x.append(x0 - DELTA)
+        h *= -1
+
+    k = 1
+    while True:
+        h *= 2
+        x.append(x[k] + h)
+        
+        if f(x[k]) > f(x[k + 1]):
+            k += 1
+        else:
+            break
+
+    return [x[k - 1], x[k + 1]]
+
+
 def main():
-    write_to_file('Dichotomy\n')
+    interval = (-2, 20)
+    # write_to_file('Dichotomy\n')
     print('Dichotomy')
-    res = search_of_min(-2, 20, dichotomy)
+    res = search_of_min(interval[0], interval[1], dichotomy)
     print('Min of f(x) is located at', res)
 
-    write_to_file('\nGolden ratio\n', 'a')
+    # write_to_file('\nGolden ratio\n', 'a')
     print('\nGolden ratio')
-    res = search_of_min(-2, 20, golden_ratio)
+    res = search_of_min(interval[0], interval[1], golden_ratio)
     print('Min of f(x) is located at', res)
 
-    write_to_file('\nFibonacci\n', 'a')
+    # write_to_file('\nFibonacci\n', 'a')
     print('\nFibonacci')
-    res = search_of_min(-2, 20, fibonacci)
+    res = search_of_min(interval[0], interval[1], fibonacci)
+    print('Min of f(x) is located at', res)
+
+    print('\nMinimum interval search')
+    res = min_interval_search(interval[0])
     print('Min of f(x) is located at', res)
 
 
